8e789e9a8b521753c7bd33d9847f863ad1b6462b
ShamKarthikS <ShamKarthik88@gmail.com>
2024-01-01 17:11:34 +0530

    updated server spawn method

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -221,7 +221,9 @@ const syncContentFromGit = async (contentDir: string) => {
 const runBashCommand = (command: string) =>
   new Promise((resolve, reject) => {
     // console.log(command);
-    const child = spawn("bash", ["-c", command]);
+    // const child = spawn("bash", ["-c", command]);
+    const child = spawn(command, [], { shell: true });
+
     // console.log(child.output);
     child.stdout.setEncoding("utf8");
     child.stdout.on("data", (data) => process.stdout.write(data));
